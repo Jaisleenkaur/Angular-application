2538db072fa91c3934860a411f830e797546e70f
Jaisleen Kaur <gujral.jaisleen@gmail.com>
2020-06-18 20:41:23 +0530

    Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import 'hammerjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
